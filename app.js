b88301986405d9ad278481cf9af5063f1efd326b
Steve <steve@Plan.D>
2018-12-20 14:15:11 -0800

    basic requirements finished

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 $(document).ready(function(){
   console.log('jQuery loaded');
-
+  var myItemsInStorage = []
   // write to local storage from input when button save clicked
   $('.btn-submit').on('click', function(){
     localStorage.setItem('inputFieldValue', $('.text-entry').val());
-    var myItemInStorage = localStorage.getItem('inputFieldValue');
-    console.log('myItemInStorage', myItemInStorage);
+    myItemsInStorage.push(' ' + localStorage.getItem('inputFieldValue'));
+    console.log('myItemsInStorage', myItemsInStorage);
 
     // display the value here
-    $('.list-display-field').text(myItemInStorage); // ??
+    $('.list-display-field').text(myItemsInStorage); // ??
 
   });
 
   // delete from local storage when delete button clicked
   $('.btn-delete').on('click', function(){
     localStorage.removeItem('inputFieldValue');
+    myItemsInStorage.pop()
+    $('.list-display-field').text(myItemsInStorage)
+  });
+
+  //delete all storage when delete button clicked
+  $('.btn-clear-all').on('click', function(){
+    myItemsInStorage = [];
+    $('.list-display-field').text(myItemsInStorage)
   });
 
 });
